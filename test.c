75352d54f0d4d19e28b160f836dc4683b50c2c71
kun <Kun.Li@etu.sorbonne-universite.fr>
2025-04-23 04:17:29 +0200

    hash

--- test.c
+++ test.c
@@ -5,7 +5,13 @@
 #include <stdio.h>
 
 int main(){
+    /*
     CPU* cpu = setup_test_environnement();
     cpu_destroy(cpu);
+    */
+    HashMap* h = hashmap_create();
+    printf("%d\n",h->table[0]);
+    //le calloc ne marche pas.
+    
     return 0;
 }
